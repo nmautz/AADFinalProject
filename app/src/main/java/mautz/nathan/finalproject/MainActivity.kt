12c9ed69738902eb5162b1c38b96d164c8be016d
nathanmautz <nmautz@zagmail.gonzaga.edu>
2021-12-13 18:35:15 -0800

    edited menu and added todos

--- app/src/main/java/mautz/nathan/finalproject/MainActivity.kt
+++ app/src/main/java/mautz/nathan/finalproject/MainActivity.kt
@@ -1,9 +1,12 @@
 package mautz.nathan.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,33 @@ class MainActivity : AppCompatActivity() {
         val menuInflater = menuInflater
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId = item.itemId
+        when (itemId) {
+            R.id.gpsMenuItem -> {
+                //TODO
+                Toast.makeText(this, "TODO: gps", Toast.LENGTH_SHORT).show()
+
+                return true // this event has been consumed/handled
+            }
+            R.id.searchMenuItem -> {
+                //TODO
+                Toast.makeText(this, "TODO: search", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.downloadMenuItem -> {
+                //TODO
+                Toast.makeText(this, "TODO: download", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.filterMenuItem -> {
+                //TODO
+                Toast.makeText(this, "TODO: filter", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
