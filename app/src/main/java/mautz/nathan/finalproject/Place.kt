01efd2bac753efeff06d4629ec30d8e590e86cef
nathanmautz <nmautz@zagmail.gonzaga.edu>
2021-12-14 15:17:19 -0800

    #28 refactored place class caused a lot of errors

--- app/src/main/java/mautz/nathan/finalproject/Place.kt
+++ app/src/main/java/mautz/nathan/finalproject/Place.kt
@@ -1,13 +1,27 @@
 package mautz.nathan.finalproject
 
 import android.graphics.Bitmap
+import java.lang.Exception
+import java.math.BigDecimal
 
-class Place(val id: Long?, var name:String, var vicinity:String, var rating:String,
-            var formatted_address:String, var review:String, var photo_ref:String?, val phone_num:String, var photo_bitmap:Bitmap?) {
+class Place(val id: Long?, var name:String, var rating:String,
+            var formatted_address:String, var review:String, val phone_num:String, val open_hours: String?, photo_ref:String?) {
+    var bitmap: Bitmap? = null
 
-
-
+    init {
+        if(photo_ref != null)
+        {
+            try{
+                val task = GooglePlacesAPI.FindPlaceImageAsyncTask()
+                bitmap = task.execute(photo_ref).get()
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
     override fun toString(): String {
         return "Place(name='$name')"
     }
+
+
 }
