d12822f1a7f731dbdf4428330a15d8088f10c937
nathanmautz <nmautz@zagmail.gonzaga.edu>
2021-12-13 18:24:58 -0800

    added menu

--- app/src/main/java/mautz/nathan/finalproject/MainActivity.kt
+++ app/src/main/java/mautz/nathan/finalproject/MainActivity.kt
@@ -2,10 +2,19 @@ package mautz.nathan.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // we need to get a MenuInflater to inflate our main_menu.xml
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
