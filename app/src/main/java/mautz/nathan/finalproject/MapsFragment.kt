04ca3e50fc74d90b3c440d565a017e648f92321d
nathanmautz <nmautz@zagmail.gonzaga.edu>
2021-12-14 22:48:32 -0800

    just need to get string address into lat long

--- app/src/main/java/mautz/nathan/finalproject/MapsFragment.kt
+++ app/src/main/java/mautz/nathan/finalproject/MapsFragment.kt
@@ -13,23 +13,36 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import mautz.nathan.finalproject.GooglePlacesAPI.location
 
 class MapsFragment : Fragment() {
 
+    var formatted_address: String? = null
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+        val placeLatLng = LatLng(GooglePlacesAPI.location.latitude, GooglePlacesAPI.location.longitude )
+        googleMap.addMarker(MarkerOptions().position(placeLatLng).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(placeLatLng))
+        googleMap.setMinZoomPreference(15F)
+        googleMap.setMaxZoomPreference(15F)
+
     }
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        if(arguments != null) {
+            val data = arguments
+            if (data != null) {
+                //Load data from arguments
+                formatted_address = data["formatted_address"] as String?
+            }
+        }
+
+
+    }
+
+
 
 
     override fun onCreateView(
